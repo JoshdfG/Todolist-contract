55f09c938f96a708492c35dd675977aad5a47288
JoshdfG <agboolajoshua161@gmail.com>
2024-02-13 16:02:33 +0100

    edited lock.ts

--- test/Lock.ts
+++ test/Lock.ts
@@ -45,4 +45,9 @@ describe("TodoList Contract", () => {
 
     expect(task.header).to.equal("New Task Header");
   });
+  it("should throw an error when editing a non-existent task", async () => {
+    await expect(
+      todoList.editTask(999, "New Task Header", "New task descriptio")
+    ).to.be.revertedWith("Task not found");
+  });
 });
